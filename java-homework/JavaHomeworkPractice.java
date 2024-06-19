6b2227379e6bacd28e1083d6616d9742dd9dff04
haeun29 <hanh291029@naver.com>
2024-06-19 21:51:47 +0900

    homework

---
+++ java-homework/JavaHomeworkPractice.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class JavaHomeworkPractice {
+    public static void main(String[] args) {
+        // 출력
+        System.out.println("Hello My nam is Haeun");
+        System.out.println("Haeun is from Myongji university");
+
+        // 입력 받고 출력
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(scanner.nextLine());
+        System.out.print("you have done your homework");
+    }
+}
